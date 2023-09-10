8a282354868896a2d155cacf637589b54aeaa347
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-09-10 19:06:34 +0200

    first commit

---
+++ src/main/java/com/techstore/TechStoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.techstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechStoreRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechStoreRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techstore/TechStoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechStoreRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
